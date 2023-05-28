baa4200a093ab8895ad98cdce4b7415f5f978286
johnatas <johnatas.barros-ext@talent.com>
2023-05-27 22:07:02 -0300

    Initial Commit

---
+++ HearthstoneAPIConsumer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HearthstoneAPIConsumer
+//
+//  Created by Johnatas Rodrigues on 27/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
